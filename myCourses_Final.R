3a56743f4a2bd2c6fcb5cc44af37012c87260008
kd365 <78327245+kd365@users.noreply.github.com>
2021-03-06 19:40:58 -0500

    Add files via upload

---
+++ myCourses_Final.R
@@ -0,0 +1,6 @@
+myCourses <- c('Spanish', 'Art', 'English', 'P.E.', 'History', 'Algebra', 'Computer Sci', 'Biology')
+length(myCourses) 
+myCourses[c(1,2)]
+myCourses[c(3,4)]
+sort.default(myCourses, decreasing = FALSE) 
+sort.default(myCourses, decreasing = TRUE) 
